ff7c9730d77cb51dcd8ed181d4be0620e9cd0271
somanytearsss <somanytearsss@yandex.ru>
2025-07-01 15:30:04 +0300

    Update test_registration.py

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,10 +1,7 @@
 import pytest
-from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from conftest import register_new_account
-from data import Credential
-from geniration_ep import EmailPasswordGenerator
 from locators import Locators
 from curl import *
 
@@ -14,19 +11,18 @@ class TestCheckNewRegister:
     def test_registration(self):
         driver, email, password = register_new_account
 
-    # Находим поле "email" и заполняем его
-    driver.find_element(*Locators.field_email).send_keys(email)
+        # Находим поле "email" и заполняем его
+        driver.find_element(*Locators.field_email).send_keys(email)
 
-    # Находим поле "Пароль" и заполняем его
-    driver.find_element(*Locators.field_password).send_keys(password)
+        # Находим поле "Пароль" и заполняем его
+        driver.find_element(*Locators.field_password).send_keys(password)
 
-    # Кликаем по кнопке "Войти"
-    driver.find_element(*Locators.button_entrance).click()
+        # Кликаем по кнопке "Войти"
+        driver.find_element(*Locators.button_entrance).click()
 
-    # Ждем перехода на главную страницу
-    WebDriverWait(driver, timeout=10).until(
-        EC.url_to_be(main_site)
-    )
+        # Ждем перехода на главную страницу
+        WebDriverWait(driver, timeout=10).until(EC.url_to_be(main_site)
+        )
 
     # Проверяем успешную авторизацию
-    assert driver.current_url == main_site
+        assert driver.current_url == main_site
