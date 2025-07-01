f6db45d27594ee135dd9326bdcae62e7f660ab31
somanytearsss <somanytearsss@yandex.ru>
2025-07-01 12:46:04 +0300

    Update conftest.py

---
+++ conftest.py
@@ -0,0 +1,92 @@
+import pytest
+from selenium import webdriver
+from curl import *
+from locators import Locators
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from geniration_ep import EmailPasswordGenerator
+from data import Credential
+
+
+@pytest.fixture  # 45 usages
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+
+@pytest.fixture  # 12 usages
+def start_from_login_page(driver):
+    login_page = login_site
+    driver.get(login_page)
+
+    # Проходим авторизацию
+    driver.find_element(*Locators.field_email).send_keys(Credential.email)
+    driver.find_element(*Locators.field_password).send_keys(Credential.password)
+    driver.find_element(*Locators.button_entrance).click()
+
+    return driver
+
+@pytest.fixture  # 2 usages
+def start_from_recovery_page(driver):
+    login_page = login_site  # Предполагается существование переменной recovery_site
+    driver.get(login_page)
+
+    # Кликаем по кнопке "восстановить пароль"
+    driver.find_element(*Locators.button_restore_password).click()
+
+    # Ждем загрузку кнопки "войти"
+    WebDriverWait(driver, timeout=6).until(
+        EC.visibility_of_element_located(Locators.inscription_button_entrance)
+    )
+
+    # Кликаем по кнопке "войти"
+    driver.find_element(*Locators.inscription_button_entrance).click()
+
+    # Проходим авторизацию
+    driver.find_element(*Locators.field_email).send_keys(Credential.email)
+    driver.find_element(*Locators.field_password).send_keys(Credential.password)
+    driver.find_element(*Locators.button_entrance).click()
+
+    return driver
+
+
+@pytest.fixture  # 2 usages
+def start_from_main_page(driver):
+    main_page = main_site
+    driver.get(main_page)
+
+    # Кликаем по кнопке "Личный кабинет"
+    driver.find_element(*Locators.button_personal_area).click()
+
+    # Кликаем по кнопке "Войти" (на форме авторизации)
+    driver.find_element(*Locators.button_entrance).click()
+
+    # Заполняем поля и проходим авторизацию
+    driver.find_element(*Locators.field_email).send_keys(Credential.email)
+    driver.find_element(*Locators.field_password).send_keys(Credential.password)
+    driver.find_element(*Locators.button_entrance).click()
+
+    return driver
+
+@pytest.fixture
+def start_from_register_page(driver):
+    register_page = register_site
+    driver.get(register_page)
+
+    # Находим надпись "Войти" и кликаем
+    driver.find_element(*Locators.inscription_button_entrance).click()
+
+    # Находим поля и проходим авторизацию
+    driver.find_element(*Locators.field_email).send_keys(Credential.email)
+    driver.find_element(*Locators.field_password).send_keys(Credential.password)
+    driver.find_element(*Locators.button_entrance).click()
+
+    return driver
+
+@pytest.fixture  # 6 usages
+def start_from_main_not_login(driver):
+    login_page = login_site
+    driver.get(login_page)
+    return driver
