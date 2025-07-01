fcf9bae521d1c57a7357981e19abfbc583112da0
somanytearsss <somanytearsss@yandex.ru>
2025-07-01 12:46:00 +0300

    Create geniration_ep.py

---
+++ geniration_ep.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+
+class EmailPasswordGenerator:  # 4 usages
+    def __init__(self):
+        self.email = None
+        self.password = None
+
+    def generate(self):  # 2 usages
+        if self.email is None and self.password is None:
+            email_length = random.randint(5, 10)
+            self.email = ''.join(random.choices(
+                string.ascii_lowercase + string.digits,
+                k=email_length
+            )) + "@example.com"
+
+            password_length = random.randint(8, 12)
+            self.password = ''.join(random.choices(
+                string.ascii_letters + string.digits,
+                k=password_length
+            ))
+
+        return self.email, self.password
